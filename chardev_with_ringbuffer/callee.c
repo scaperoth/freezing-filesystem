6166cc733520969ae1d681b9944f3ee520a655c1
scaperoth <mscapero@gwu.edu>
2013-12-09 02:28:29 -0500

    Adding wait queues...

--- chardev_with_ringbuffer/callee.c
+++ chardev_with_ringbuffer/callee.c
@@ -9,12 +9,15 @@ int main(int argc, char *argv[]){
 	
 	system(command);
 	*/
-	FILE *f = fopen("/root/Desktop/scaperoth_csci3411/freezing_filesystem/chardev_with_ringbuffer/log.txt", "rw");
+	printf("Opening file...\n");
+	FILE *f = fopen("/root/Desktop/scaperoth_csci3411/freezing_filesystem/chardev_with_ringbuffer/log.txt", "w");
 	if (f == NULL)
 	{
 		printf("\nError opening file!\n");
 		return 0;
-	}
+	}	
+
+	printf("Writing to file...\n");
 
 /* print integers and floats */
 	int i = 1;
@@ -25,6 +28,7 @@ int main(int argc, char *argv[]){
 	char c = 'A';
 	fprintf(f, "A character: %c\n", c);
 
+	printf("Closing file.\n");
 	fclose(f);
 
 
